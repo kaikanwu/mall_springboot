ca66144563e11eaef11400d1d367780f428a27b7
kaikanwu <wukaikan@gmail.com>
2018-12-26 22:33:14 +0800

    update

---
+++ src/main/java/com/k/mall_springboot/MallSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.k.mall_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallSpringbootApplication.class, args);
+	}
+
+}
+
